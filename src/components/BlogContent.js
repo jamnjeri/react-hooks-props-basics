c337c7f9db4f0945f1f90b55f20884f0e8cea177
Jamila <jamnjeri@gmail.com>
2023-01-10 15:17:59 +0300

    Completed Assugnment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props);
+
+  if (!props.isPublished) {
+    //Hide unpublished content
+    //Return null means "don't display any DOM elements here"
+    return null;
+  } else {
+    //Show published content
+    return (
+      <div id="blog-content">
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    );
+  }
 }
 
 export default BlogContent;
